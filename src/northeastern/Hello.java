88d71e6abfb93f760d4dd6a83162339a347ac353
Jiaming Pei <jiamingpei16@gmail.com>
2025-01-12 15:11:51 -0800

    second

--- src/northeastern/Hello.java
+++ src/northeastern/Hello.java
@@ -3,5 +3,6 @@ package northeastern;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello, Northeastern!");
+    System.out.println("Hello, world!");
   }
 }
